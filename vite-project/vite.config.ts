7be5fb66e77c57d726edb01f37cbdcf492f3de89
rifqirafialdy <rifqirafialdy@gmail.com>
2025-03-03 15:20:07 +0700

    route fix

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -4,13 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist"
+    outDir: "dist",
   },
-  server: {
-    port: 3000
-  },
-  base: "/",
-  define: {
-    "process.env": {} // Fixes potential env issues
-  }
+  base: "/", // This ensures correct paths on Vercel
 });
